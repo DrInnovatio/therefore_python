639c0c069c02e03e674c7cc59ee830c3c96135cd
DrInnovatio <turing1723@gmail.com>
2021-04-01 23:11:12 +1000

    added instructions.

---
+++ unit_12/guess_the_number.py
@@ -0,0 +1,12 @@
+# Choosing a random number between 1 and 100.
+
+# Make function to set difficulty.
+
+# Let the user guess a number.
+
+# Function to check user's guess again actual answer.
+
+# Track the number of turns and reduce by 1 if they get it wrong.
+
+# Repeat the guessing functionality if they get it wrong.
+
