e62a9306b7909d9fd1510eecf024fa8515c16b77
DrInnovatio <turing1723@gmail.com>
2021-04-02 15:51:40 +1000

    get this done almost half of the plan.

--- unit_12/guess_the_number.py
+++ unit_12/guess_the_number.py
@@ -1,12 +1,52 @@
-# Choosing a random number between 1 and 100.
+from random import randint
 
-# Make function to set difficulty.
-
-# Let the user guess a number.
-
+EASY_LEVEL_TURNS = 10
+HARD_LEVEL_TURNS = 5
 # Function to check user's guess again actual answer.
 
-# Track the number of turns and reduce by 1 if they get it wrong.
 
-# Repeat the guessing functionality if they get it wrong.
+def check_answer(guess, answer, turns):
+    """checks answer against guess. Return the number of turns remaining."""
+    if guess > answer:
+        print("Too High !!")
+        return turns - 1
+    elif guess < answer:
+        print("Too Low !!")
+        return turns - 1
+    else:
+        print(f"You got it! The answer is {answer}")
 
+        # Choosing a random number between 1 and 100.
+
+
+def set_difficulty():
+    level = input("Choose a fifficulty. Type 'easy or 'hard : ")
+    if level == 'easy':
+        # global turns
+        return EASY_LEVEL_TURNS
+    else:
+        return HARD_LEVEL_TURNS
+
+
+def game():
+
+    print("Whlcome to the guessing game!!")
+    print("I'm thinking of a number between 1 and 100.")
+    answer = randint(1, 100)
+    print(f"Pssst, the correct answer is {answer}")
+
+    turns = set_difficulty()
+    print(f"You have {turns} attempts remaining to guess the number. ")
+    # Make function to set difficulty.
+
+    # Let the user guess a number.
+    guess = 0
+    while guess != answer:
+        guess = int(input("Make a guess : "))
+        turns = check_answer(guess, answer, turns)
+        # Track the number of turns and reduce by 1 if they get it wrong.
+
+        # Repeat the guessing functionality if they get it wrong.
+
+
+game()
