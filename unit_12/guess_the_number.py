2c9b7c9c214ccc60dfc56038aeb630b0db0126e3
DrInnovatio <turing1723@gmail.com>
2021-04-03 23:20:20 +1000

    got this project done.

--- unit_12/guess_the_number.py
+++ unit_12/guess_the_number.py
@@ -42,8 +42,15 @@ def game():
     # Let the user guess a number.
     guess = 0
     while guess != answer:
+        print(f"You have {turns} attempts remaining to guess the number.")
         guess = int(input("Make a guess : "))
         turns = check_answer(guess, answer, turns)
+
+        if turns == 0:
+            print("You've run out of guesses, you lose.")
+            return
+        elif guess != answer:
+            print("Guess Again!!")
         # Track the number of turns and reduce by 1 if they get it wrong.
 
         # Repeat the guessing functionality if they get it wrong.
