ae4bfb081bf8705a15edc99086a7ac795fef0a78
DrInnovatio <turing1723@gmail.com>
2021-04-05 11:35:01 +1000

    debugging_2

--- unit_13/bug.py
+++ unit_13/bug.py
@@ -1,9 +1,28 @@
-def my_funtion():
-    for i in range(1, 21):
-        print(i)
-        if i == 20:
+# def my_funtion():
+#     for i in range(1, 21):
+#         print(i)
+#         if i == 20:
 
-            print("YOu got it")
+#             print("YOu got it")
 
 
-my_funtion()
+# my_funtion()
+
+# year = int(input("What is yur year of birth?  : "))
+# if year > 1980 and year < 1994:
+#     print("Your are a millenial.")
+# elif year >= 1994:
+#     print("You are a Gen Z.")
+
+
+# age = int(input("How old are you? "))
+# if age > 18:
+#     print(f"You can drive at age {age}.")
+
+
+pages = 0
+word_per_page = 0
+pages = int(input("Number of words per page : "))
+word_per_page = int(input("Number per page : "))
+total_words = pages * word_per_page
+print(total_words)
