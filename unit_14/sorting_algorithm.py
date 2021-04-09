b7628d5478e3ba10dc92c8a0e77358dc3ee930f5
DrInnovatio <turing1723@gmail.com>
2021-04-09 23:10:07 +1000

    bubblesort

--- unit_14/sorting_algorithm.py
+++ unit_14/sorting_algorithm.py
@@ -30,3 +30,19 @@ if __name__ == "__main__":
     # Call the function using the name of the sorting algorithm
     # and the array you just created
     run_sorting_algorithm(algorithm="sorted", array=array)
+
+
+def bubblesort(list):
+
+    # Swap the elements to arrange in order
+    for iter_num in range(len(list)-1, 0, -1):
+        for idx in range(iter_num):
+            if list[idx] > list[idx+1]:
+                temp = list[idx]
+                list[idx] = list[idx+1]
+                list[idx+1] = temp
+
+
+list = [19, 2, 31, 45, 6, 11, 121, 27]
+bubblesort(list)
+print(list)
