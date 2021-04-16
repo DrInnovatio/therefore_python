5dab5c7001c0e58ec5460c9200c67da889b525fa
DrInnovatio <turing1723@gmail.com>
2021-04-16 23:50:47 +1000

    added def format_data():

--- unit_14/lower_higher.py
+++ unit_14/lower_higher.py
@@ -11,10 +11,14 @@ account_b = random.choice(data)
 if account_a == account_b:
     account_b = random.choice(data)
 # Format the account data into printable format.
-account_name = account_a["name"]
-account_descr = account_a["description"]
-account_country = account_a["country"]
-print(f"{account_name}, a {account_descr}, from {account_country}")
+
+
+def format_data(account):
+
+    account_name = account_a["name"]
+    account_descr = account_a["description"]
+    account_country = account_a["country"]
+    print(f"{account_name}, a {account_descr}, from {account_country}")
 # Ask user for a guess.
 
 # Check if user is correct.
