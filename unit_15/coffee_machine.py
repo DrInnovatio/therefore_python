db1d440aa1007422791c3d455f9961e94616ded5
DrInnovatio <turing1723@gmail.com>
2021-04-23 23:50:11 +1000

    got the while loop done.

--- unit_15/coffee_machine.py
+++ unit_15/coffee_machine.py
@@ -25,6 +25,7 @@ MENU = {
 }
 
 profit = 0
+
 resources = {
     "water": 300,
     "milk": 200,
@@ -37,3 +38,11 @@ while is_on:
     choice = input("What would you like ? (Espresso / Latte / Cappuccino )")
     if choice == "off":
         is_on = False
+    elif choice == "report":
+        print(f"Water : {resources['water']}ml")
+        print(f"Milk : {resources['milk']}ml")
+        print(f"Coffee : {resources['coffee']}g")
+        print(f"Money : ${profit}")
+    else:
+        drink = MENU[choice]
+        print(drink)
