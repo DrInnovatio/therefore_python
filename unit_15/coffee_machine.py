8b5acb8b1ab60cf6437f3b44e5d5785987362cbf
DrInnovatio <turing1723@gmail.com>
2021-04-27 11:11:01 +1000

    added is_transaction_successful().

--- unit_15/coffee_machine.py
+++ unit_15/coffee_machine.py
@@ -45,10 +45,28 @@ def process_coins():
     """Returns the total calculated from coins inserted."""
     print("Please insert coins.")
     total = int(input("How many quarters? : ")) * 0.25
-    total = int(input("How many dimes? : ")) * 0.1
-    total = int(input("How many nickles? : ")) * 0.05
-    total = int(input("How many pennines? : ")) * 0.01
+    total += int(input("How many dimes? : ")) * 0.1
+    total += int(input("How many nickles? : ")) * 0.05
+    total += int(input("How many pennines? : ")) * 0.01
     return total
+
+
+def is_transaction_successful(money_received, drink_cost):
+    if money_received >= drink_cost:
+        change = round(money_received - drink_cost, 2)
+        print(f"Here is ${change} in change.")
+        global profit
+        profit += drink_cost
+        return True
+    else:
+        print("Sorry that's not enough money. Money refunded.")
+        return False
+
+
+def make_coffee(drink_name, order_ingredients):
+    for item in order_ingredients:
+        resources[item] -= order_ingredients[item]
+    print(f"Here is your {drink_name} ☕ Enjoy!!")
 
 
 is_on = True
@@ -66,3 +84,7 @@ while is_on:
         drink = MENU[choice]
         if is_resource_sufficient(drink["ingredients"]):
             payment = process_coins()
+            if is_transaction_successful(payment, drink["cost"]):
+                make_coffee(choice, drink["ingredients"])
+
+# need to fix the code.
