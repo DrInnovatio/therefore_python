035f9313bf17801a8a11d7a3770001f5401231a0
DrInnovatio <turing1723@gmail.com>
2021-05-01 14:53:56 +1000

    Turtle graphic

--- unit_16/main.py
+++ unit_16/main.py
@@ -1,8 +1,12 @@
 import another_module
-from turtle import Turtle
+from turtle import Turtle, Screen
 
 print(another_module.another_variable)
 print(another_module)
 
 timmy = Turtle()
 print(timmy)
+timmy.shape("turtle")
+my_screen = Screen()
+print(my_screen.canvheight)
+my_screen.exitonclick()
