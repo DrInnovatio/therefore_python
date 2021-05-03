395e8a15257271561582561f8b2eec72bae04e05
DrInnovatio <turing1723@gmail.com>
2021-05-03 11:56:24 +1000

    use of prettytable package.

--- unit_16/main.py
+++ unit_16/main.py
@@ -1,14 +1,22 @@
-import another_module
-from turtle import Turtle, Screen
+# import another_module
+# from turtle import Turtle, Screen
+#
+# print(another_module.another_variable)
+# print(another_module)
+#
+# timmy = Turtle()
+# print(timmy)
+# timmy.shape("turtle")
+# timmy.color("red")
+# timmy.forward(300)
+# my_screen = Screen()
+# print(my_screen.canvheight)
+# my_screen.exitonclick()
 
-print(another_module.another_variable)
-print(another_module)
+from prettytable import PrettyTable
 
-timmy = Turtle()
-print(timmy)
-timmy.shape("turtle")
-timmy.color("red")
-timmy.forward(300)
-my_screen = Screen()
-print(my_screen.canvheight)
-my_screen.exitonclick()
+table = PrettyTable()
+table.add_column("Pokemon Name", ["Pikachu", "Squirtle", "Charmander"])
+table.add_column("Type", ["Electric", "Water", "Fire"])
+table.align = "c"
+print(table)
