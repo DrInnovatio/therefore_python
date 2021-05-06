4bae434b78bb67d8b46de786302e25df5b09f616
DrInnovatio <turing1723@gmail.com>
2021-05-06 23:53:18 +1000

    constructors.

--- unit_17/main.py
+++ unit_17/main.py
@@ -2,12 +2,13 @@ class User:
     def __init__(self, user_id, username):
         self.id = user_id
         self.username = username
+        self.follower = 0
 
 
 user_1 = User("0012", "Brandon")
 
 
-print(user_1.id)
+print(user_1.follower)
 
 
 
