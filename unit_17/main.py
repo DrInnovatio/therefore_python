2e9a6fc172d4bba193dc620cf1a4775e26cbdb54
DrInnovatio <turing1723@gmail.com>
2021-05-08 19:02:01 +1000

    added def follower().

--- unit_17/main.py
+++ unit_17/main.py
@@ -1,14 +1,21 @@
 class User:
-    def __init__(self, user_id, username):
+    def __init__(self, user_id, username, follower):
         self.id = user_id
         self.username = username
         self.follower = 0
+        self.following = 0
+
+    def follow(self, user):
+        user.followers += 1
+        self.followers += 1
 
 
 user_1 = User("0012", "Brandon")
+user_2 = User("00132", "Jenna")
 
 
-print(user_1.follower)
-
-
-
+user_1.follow(user_2)
+print(user_1.followers)
+print(user_1.following)
+print(user_2.followers)
+print(user_2.following)
