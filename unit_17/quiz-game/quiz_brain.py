c537a9d2a577f3769cb93a2be77e6c3ee45a9795
DrInnovatio <turing1723@gmail.com>
2021-05-17 11:19:19 +1000

    added score variant.

--- unit_17/quiz-game/quiz_brain.py
+++ unit_17/quiz-game/quiz_brain.py
@@ -1,6 +1,7 @@
 class QuizBrain:
     def __init__(self, q_list):
         self.question_number = 0
+        self.score = 0
         self.question_list = q_list
 
     def still_has_questions(self):
@@ -15,7 +16,10 @@ class QuizBrain:
 
     def check_answer(self, user_answer, correct_answer):
         if user_answer == correct_answer.lower():
+            self.score += 1
             print("You got it right!!")
         else:
             print("That's wrong!!")
         print(f"The correct answer was : {correct_answer}")
+        print(f"Your current score is : {self.score} / {self.question_number}")
+        
