36adb8c38dff4bca3dae489fd8204d4fac7dee53
DrInnovatio <turing1723@gmail.com>
2021-05-25 13:19:19 +1000

    penup

--- unit_18/main.py
+++ unit_18/main.py
@@ -1,23 +1,23 @@
 import turtle as t
 
-<<<<<<< HEAD
 tim = t.Turtle()
 
-tim.shape("turtle")
-tim.color("green")
-tim.forward(200)
-tim.left(90)
-tim.forward(200)
-tim.left(90)
-tim.forward(200)
-tim.left(90)
-tim.forward(200)
-t.done()
+# tim.shape("turtle")
+# tim.color("green")
+# tim.forward(200)
+# tim.left(90)
+# tim.forward(200)
+# tim.left(90)
+# tim.forward(200)
+# tim.left(90)
+# tim.forward(200)
+# t.done()
 
 # import heroes
 # print(heroes.gen())
-=======
-timmy_the_turtle = Turtle()
-timmy_the_turtle.shape("turtle")
-timmy_the_turtle.color("blue")
->>>>>>> 1e4619a465a11c0cdae4164c87c5a3eefa85e110
+
+for _ in range(15):
+    tim.forward(10)
+    tim.penup()
+    tim.forward(10)
+    tim.penup()
