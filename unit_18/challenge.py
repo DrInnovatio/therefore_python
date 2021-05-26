c7765ddc0096337dd97784090a987504e37022e4
DrInnovatio <turing1723@gmail.com>
2021-05-26 16:54:55 +1000

    added draw_shape()

--- unit_18/challenge.py
+++ unit_18/challenge.py
@@ -2,11 +2,21 @@ import turtle as t
 
 tim = t.Turtle()
 
-num_sides = 5
-angle = 360 / num_sides
+# num_sides = 5
+# angle = 360 / num_sides
 
-for _ in range(num_sides):
-    tim.forward(100)
-    tim.right(angle)
+# for _ in range(num_sides):
+#     tim.forward(100)
+#     tim.right(angle)
 
+
+def draw_shape(num_sides):
+    angle = 360 / num_sides
+    for _ in range(num_sides):
+        tim.forward(100)
+        tim.right(angle)
+
+
+for shape_side_n in range(3, 11):
+    draw_shape(shape_side_n)
 t.done()
