40540e7841582157e16645fc87d00ed8da6dd14e
DrInnovatio <turing1723@gmail.com>
2021-05-28 18:49:29 +1000

    added the color option to the program.

--- unit_18/random_walk.py
+++ unit_18/random_walk.py
@@ -8,5 +8,6 @@ colours = ["CornflowerBlue", "DarkOrchid", "IndianRed",
 directions = [0, 90, 180, 270]
 
 for _ in range(200):
+    tim.color(random.choice(colours))
     tim.forward(30)
     tim.setheading(random.choice(directions))
