4fb6501d4bf6f12347766cf9f5b725de31986ade
DrInnovatio <turing1723@gmail.com>
2021-06-05 19:34:09 +1000

    got the project done.

--- unit_18/spirograph.py
+++ unit_18/spirograph.py
@@ -18,12 +18,13 @@ tim.shape("turtle")
 
 
 def draw_spirograph(size_of_gap):
-    for _ in range(360):
+    for _ in range(360 / size_of_gap):
         tim.color(random_color())
         tim.circle(100)
-        tim.setheading(tim.heading() + 10)
+        tim.setheading(tim.heading() + size_of_gap)
 
 
+draw_spirograph(5)
 screen = t.Screen()
 screen.exitonclick()
 
