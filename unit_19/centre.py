5d1eb24e0dbe43a64e67a2089502b6e0ba1ea0ca
DrInnovatio <turing1723@gmail.com>
2021-06-19 23:43:09 +1000

    set up an prompt window.

--- unit_19/centre.py
+++ unit_19/centre.py
@@ -1,7 +1,11 @@
 from turtle import Turtle, Screen
 
-tim = Turtle()
-screen = Screen()
-screen.setup(500, 400)
 
+screen = Screen()
+screen.setup(width=500, height=400)
+user_bet = screen.textinput(title="Make your bets", prompt="Which turtle will win the race? Enter a color : ")
+print(user_bet)
+
+
+tim = Turtle()
 screen.exitonclick()
