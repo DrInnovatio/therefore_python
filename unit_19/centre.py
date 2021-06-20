757bff2dbf4e6cf7800d6af187362e3760a799cc
DrInnovatio <turing1723@gmail.com>
2021-06-20 19:12:03 +1000

    added forLoop.

--- unit_19/centre.py
+++ unit_19/centre.py
@@ -5,9 +5,12 @@ screen = Screen()
 screen.setup(width=500, height=400)
 user_bet = screen.textinput(title="Make your bets", prompt="Which turtle will win the race? Enter a color : ")
 colors = ["red", "orange", "yellow", "green", "blue", "purple"]
+y_positions = [-70, -40, -10, 20, 50, 80]
 
+for turtle_index in range(0, 6):
+    tim = Turtle(shape="turtle")
+    tim.color(colors[turtle_index])
+    tim.penup()
+    tim.goto(x=-230, y=y_positions)
 
-tim = Turtle(shape="turtle")
-tim.penup()
-tim.goto(x=-230, y=-100)
 screen.exitonclick()
