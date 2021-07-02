c889059234d4ef29013ec5b871fa52c161d646d7
DrInnovatio <turing1723@gmail.com>
2021-07-02 17:47:56 +1000

    made class Snake: on snake.py

--- unit_20/main.py
+++ unit_20/main.py
@@ -9,13 +9,7 @@ screen.tracer(0)
 
 segments = []
 
-# Created a snake.
-for position in starting_positions:
-    new_segment = Turtle("square")
-    new_segment.color("white")
-    new_segment.penup()
-    new_segment.goto(position)
-    segments.append(new_segment)
+
 
     
 game_is_on = True
@@ -23,10 +17,6 @@ while game_is_on:
     screen.update()
     time.sleep(0.1)
     # Moves the snake.
-    for seg_num in range(len(segments) - 1, 0, -1):
-        new_x = segments[seg_num - 1].xcor()
-        new_y = segments[seg_num - 1].ycor()
-        segments[seg_num].goto(new_x, new_y)
-    segments[0].forward(20)
+
 
 screen.exitonclick()
