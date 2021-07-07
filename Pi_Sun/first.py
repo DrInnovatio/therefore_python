9295f5bc6c922e0d5aec1ef532f6d495d3430a9b
DrInnovatio <turing1723@gmail.com>
2021-07-07 23:39:33 +1000

    Done with the movement code.

---
+++ Pi_Sun/first.py
@@ -0,0 +1,17 @@
+def greet_user(username):
+    print("Hello, " + username.title() + "!")
+
+
+greet_user("Karl")
+
+
+def area(height, width):
+    result = height * width
+    print(f"The area is {result}.")
+
+
+area(12.3, 11.01)
+
+
+
+
