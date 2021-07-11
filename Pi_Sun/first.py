b93d601306dd63e7b22b6fd5985ca47624ac76f2
DrInnovatio <turing1723@gmail.com>
2021-07-11 23:18:55 +1000

    If the number of arguments is unknown, add a * before the parameter name:

--- Pi_Sun/first.py
+++ Pi_Sun/first.py
@@ -58,3 +58,12 @@ def create_people(first_name, last_name):
 
 
 actor = create_people("John", "Dixie")
+
+
+#If the number of arguments is unknown, add a * before the parameter name:
+
+def my_function(*kids):
+    print("The youngest child is " + kids[2])
+
+
+my_function("Emil", "Tobias", "Linus")
