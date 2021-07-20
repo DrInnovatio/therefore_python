c7498f9f002720111b1d370e7fe368fa7b961155
DrInnovatio <turing1723@gmail.com>
2021-07-20 18:33:59 +1000

    the font of the scoreborad.

--- unit_21/scoreboard.py
+++ unit_21/scoreboard.py
@@ -6,5 +6,10 @@ class Scoreboard(Turtle):
     def __init__(self):
         super().__init__()
         self.score = 0
+        self.color("white")
+        self.penup()
+        self.goto(0, 270)
         self.write(f"Score: {self.score}", align="center", font=("arial", 24, "normal"))
+        self.hideturtle()
+
 
