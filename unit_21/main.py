8a2cf40b238d07b4b210d33b0dfe1770275b478a
DrInnovatio <turing1723@gmail.com>
2021-07-23 23:28:38 +1000

    The score board works very well.

--- unit_21/main.py
+++ unit_21/main.py
@@ -1,4 +1,4 @@
-from turtle import Screen, Turtle
+from turtle import Screen
 from snake import Snake
 from food import Food
 from scoreboard import Scoreboard
@@ -24,12 +24,12 @@ game_is_on = True
 while game_is_on:
     screen.update()
     time.sleep(0.1)
-
     snake.move()
 
     # Detect collision with food.
     if snake.head.distance(food) < 15:
         food.refresh()
+        scoreboard.increase_score()
 
 
 screen.exitonclick()
