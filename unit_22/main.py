4eab91c58a45444a0f1ab80d4fae053e8bdea7a9
DrInnovatio <turing1723@gmail.com>
2021-07-28 22:31:24 +1000

    created a screen a paddle.

--- unit_22/main.py
+++ unit_22/main.py
@@ -1,10 +1,14 @@
-from turtle import Screen
+from turtle import Screen, Turtle
 
 screen = Screen()
 screen.bgcolor("black")
 screen.setup(width=800, height=600)
 screen.title("PONG")
 
+paddle = Turtle()
+paddle.shape("square")
+paddle.color("white")
+paddle.shapesize(stretch_wid=5, stretch_len=1)
 
 screen.exitonclick()
 
