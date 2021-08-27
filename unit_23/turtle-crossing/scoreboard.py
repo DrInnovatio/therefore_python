d1a879d21378ee3167fc96a40a15afff48c98203
DrInnovatio <turing1723@gmail.com>
2021-08-27 22:27:11 +1000

    added def increase_level(self):

--- unit_23/turtle-crossing/scoreboard.py
+++ unit_23/turtle-crossing/scoreboard.py
@@ -11,9 +11,11 @@ class Scoreboard(Turtle):
         self.hideturtle()
         self.penup()
         self.goto(-280, 250)
+        self.update_scoreboard()
 
     def update_scoreboard(self):
         self.write(f"Level : {self.level}", align="left", font=FONT)
 
     def increase_level(self):
         self.level += 1
+        self.update_scoreboard()
