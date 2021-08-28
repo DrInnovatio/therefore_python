efd2ac9604e5e0a125af406332c4378159a93f78
DrInnovatio <turing1723@gmail.com>
2021-08-28 22:19:24 +1000

    showing GAME OVER message.

--- unit_23/turtle-crossing/scoreboard.py
+++ unit_23/turtle-crossing/scoreboard.py
@@ -14,8 +14,13 @@ class Scoreboard(Turtle):
         self.update_scoreboard()
 
     def update_scoreboard(self):
+        self.clear()
         self.write(f"Level : {self.level}", align="left", font=FONT)
 
     def increase_level(self):
         self.level += 1
         self.update_scoreboard()
+
+    def game_over(self):
+        self.goto(0, 0)
+        self.write(f"GAME OVER", align="center", font=FONT)
