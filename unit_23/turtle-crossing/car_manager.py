e399072cd75c17bff5c927b5a6307668263c1900
DrInnovatio <turing1723@gmail.com>
2021-08-31 23:36:51 +1000

    setting the cars coming from the random function.

--- unit_23/turtle-crossing/car_manager.py
+++ unit_23/turtle-crossing/car_manager.py
@@ -10,14 +10,16 @@ class CarManager:
     def __init__(self):
         self.all_cars = []
 
-    def creat_car(self):
-        new_car = Turtle("square")
-        new_car.shapesize(stretch_wid=2, stretch_len=1)
-        new_car.penup()
-        new_car.color(random.choice(COLORS))
-        random_y = random.randint(-250, 250)
-        new_car.goto(300, random_y)
-        self.all_cars.append(new_car)
+    def create_car(self):
+        random_chance = random.randint(1, 6)
+        if random_chance == 1:
+            new_car = Turtle("square")
+            new_car.shapesize(stretch_wid=2, stretch_len=1)
+            new_car.penup()
+            new_car.color(random.choice(COLORS))
+            random_y = random.randint(-250, 250)
+            new_car.goto(300, random_y)
+            self.all_cars.append(new_car)
 
     def move_cars(self):
         for car in self.all_cars:
