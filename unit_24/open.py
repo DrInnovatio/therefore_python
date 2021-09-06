5b74f44737c861d6d3fc1c191fbcadcfc1e02ff4
DrInnovatio <turing1723@gmail.com>
2021-09-06 12:41:01 +0900

    how to create a new file.

--- unit_24/open.py
+++ unit_24/open.py
@@ -1,6 +1,9 @@
-with open("my_file.txt") as file:
-    contents = file.read()
-    print(contents)
+# with open("my_file.txt") as file:
+#     contents = file.read()
+#     print(contents)
+#
+# with open("new_file.txt", mode="w") as file:
+#     file.write("New Text.")
 
 with open("my_file.txt", mode="w") as file:
     file.write("New Text.")
