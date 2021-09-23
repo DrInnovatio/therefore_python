a48a69477799f89bbb8ad42f81596f5922375ef5
DrInnovatio <turing1723@gmail.com>
2021-09-23 23:46:13 +0900

    counting the gray squirrels.

--- unit_25/main_squirrel.py
+++ unit_25/main_squirrel.py
@@ -4,3 +4,6 @@ import pandas
 data = pandas.read_csv("Squirrel_Data.csv")
 gray_squirrel = data[data["Primary Fur Color"] == "Gray"]
 print(gray_squirrel)
+
+gray_squirrel_count = len(data[data["Primary Fur Color"] == "Gray"])
+print(gray_squirrel_count)
