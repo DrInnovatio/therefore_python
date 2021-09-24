8d3471055c565d954a0f73b8749b8d555249996f
DrInnovatio <turing1723@gmail.com>
2021-09-24 18:51:29 +0900

    created a table about the colors of the squirrels.

--- unit_25/main_squirrel.py
+++ unit_25/main_squirrel.py
@@ -6,4 +6,16 @@ gray_squirrel = data[data["Primary Fur Color"] == "Gray"]
 print(gray_squirrel)
 
 gray_squirrel_count = len(data[data["Primary Fur Color"] == "Gray"])
+red_squirrel_count = len(data[data["Primary Fur Color"] == "Cinnamon"])
+black_squirrel_count = len(data[data["Primary Fur Color"] == "Black"])
 print(gray_squirrel_count)
+print(red_squirrel_count)
+print(black_squirrel_count)
+
+data_dict = {
+    "Fur Color": ["Gray", "Cinnamon", "Black"],
+    "Count": [gray_squirrel_count, red_squirrel_count, black_squirrel_count]
+}
+df = pandas.DataFrame(data_dict)
+df.to_csv("squirrel_count.csv")
+print(df)
