3f676c4894fb8dd8947aefad74fc8c1aeffd4b62
DrInnovatio <turing1723@gmail.com>
2021-09-26 21:29:01 +0900

    setting up the question and answer window.

--- unit_25/us-states-game-start/main.py
+++ unit_25/us-states-game-start/main.py
@@ -4,13 +4,19 @@ screen.title("U.S State Game")
 image = "blank_states_img.gif"
 screen.addshape(image)
 turtle.shape(image)
+
+# don't need this any more.
+# def get_mouse_click_coor(x, y):
+#     print(x, y)
+#
+#
+# turtle.onscreenclick(get_mouse_click_coor)
+#
+# turtle.mainloop()
+
+answer_state = screen.textinput(title="Guess the State", prompt="What's another state's name?")
+print(answer_state)
+
+
+
 screen.exitonclick()
-
-
-def get_mouse_click_coor(x, y):
-    print(x, y)
-
-
-turtle.onscreenclick(get_mouse_click_coor)
-
-turtle.mainloop()
