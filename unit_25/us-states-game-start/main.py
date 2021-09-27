c3083b8a529cf865646a1efb668f11236e185bdd
DrInnovatio <turing1723@gmail.com>
2021-09-27 22:48:11 +0900

    setting up the function to judge the answer.

--- unit_25/us-states-game-start/main.py
+++ unit_25/us-states-game-start/main.py
@@ -30,7 +30,7 @@ if answer_state in all_states:
     t.hideturtle()
     t.penup()
     state_data = data[data.state == answer_state]
-    t.goto(state_data.x, state_data.y)
+    t.goto(int(state_data.x), int(state_data.y))
     t.write(state_data.state)
 
 screen.exitonclick()
