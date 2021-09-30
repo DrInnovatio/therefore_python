f415b444685e37c4a6281c152d4b184fb7b9916f
DrInnovatio <turing1723@gmail.com>
2021-09-30 23:19:32 +0900

    exit the game.

--- unit_25/us-states-game-start/main.py
+++ unit_25/us-states-game-start/main.py
@@ -27,14 +27,17 @@ print(answer_state)
     # create a turtle to write the name of the state at the state's x and y coordate.
 
 while len(guessed_states) < 50:
-    answer_state = screen.textinput(title=f"{len(guessed_states)}/50 States Correct", prompt="What's another state's name?")
-
+    answer_state = screen.textinput(title=f"{len(guessed_states)}/50 States Correct",
+                                    prompt="What's another state's name?").title()
+    if answer_state == "Exit":
+        break
     if answer_state in all_states:
+        guessed_states.append(answer_state)
         t = turtle.Turtle()
         t.hideturtle()
         t.penup()
         state_data = data[data.state == answer_state]
         t.goto(int(state_data.x), int(state_data.y))
-        t.write(state_data.state.item())
+        t.write(answer_state)
 
 screen.exitonclick()
