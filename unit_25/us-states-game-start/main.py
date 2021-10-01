4ed0f1ffced9b4b4d86ab66ca93ced75380e9315
DrInnovatio <turing1723@gmail.com>
2021-10-01 22:45:22 +0900

    creating a new file to learn all states I don't know.

--- unit_25/us-states-game-start/main.py
+++ unit_25/us-states-game-start/main.py
@@ -30,6 +30,12 @@ while len(guessed_states) < 50:
     answer_state = screen.textinput(title=f"{len(guessed_states)}/50 States Correct",
                                     prompt="What's another state's name?").title()
     if answer_state == "Exit":
+        missing_states = []
+        for state in all_states:
+            if state not in guessed_states:
+                missing_states.append(state)
+        new_data = pandas.DataFrame(missing_states)
+        new_data.to_csv("rest_of_states.csv")
         break
     if answer_state in all_states:
         guessed_states.append(answer_state)
@@ -39,5 +45,7 @@ while len(guessed_states) < 50:
         state_data = data[data.state == answer_state]
         t.goto(int(state_data.x), int(state_data.y))
         t.write(answer_state)
+
+# states_to_learn.csv
 
 screen.exitonclick()
